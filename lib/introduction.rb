0a9b547c3ba8553f9b4fe31269854e9a1b6dd169
daisy Kiyemba <deedaisy29@gmail.com>
2019-08-10 21:43:36 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,12 +5,12 @@
 
 def introduction(name)
 
-puts "Hi, my name is{daisy}."
+puts "Hi, my name is#{name}."
 
 end
 
-def introduction(name)
-puts "Hi, my name is #{name}."
+def introduction_with_language(name , language)
+puts "Hi, my name is #{name} and l am learning to program in #{language}."
 end
 
 
