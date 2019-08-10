ff4f138954d603cfd16dcb840ca2b0680db651aa
daisy Kiyemba <deedaisy29@gmail.com>
2019-08-10 21:48:04 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,18 +4,18 @@
 # in order to complete the rest of this course and be ready for the next course!
 
 def introduction(name)
-
-puts "Hi, my name is#{name}."
-
+puts "Hi, my name is #{name}."
 end
+
 
 def introduction_with_language(name , language)
 puts "Hi, my name is #{name} and l am learning to program in #{language}."
 end
 
 
-
-
+def introduction_with_language_optional(name, language)
+puts "Hi, my name is #{name} and l am leaning to program in #{Ruby}"
+end
 
 
 
